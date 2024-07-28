1f4af06cb470bc90a0d949b5596a2a95c690367b
Kasra <kasra.ghasemi@evolute.app>
2024-07-28 16:19:33 +0200

    feat: 🎸 add basic add post

--- app/_components/AddPost/index.tsx
+++ app/_components/AddPost/index.tsx
@@ -1,5 +1,72 @@
+import Image from "next/image"
 import React from "react"
 
+import faker from "@/_utils/fakerConfig"
+
 export const AddPost = () => {
-	return <div>AddPost</div>
+	return (
+		<div className="flex justify-between gap-4 rounded-lg bg-white p-4 text-sm shadow-md">
+			{/*Avatar*/}
+			<Image
+				alt="avatar"
+				className="h-12 w-12 rounded-full"
+				height={48}
+				src={faker.image.urlPicsumPhotos()}
+				width={48}
+			/>
+			{/*Post*/}
+			<div className="flex-1">
+				{/*Text input*/}
+				<div className="flex gap-4">
+					<textarea
+						className="flex-1 rounded-lg bg-slate-100 p-2"
+						id=""
+						name=""
+						placeholder={"What is on your mind?"}
+					></textarea>
+					<Image
+						alt="emoji"
+						className="h-5 w-5 cursor-pointer self-end"
+						height={20}
+						src={"/emoji.png"}
+						width={20}
+					/>
+				</div>
+				{/*Post option*/}
+				<div className="mt-4 flex flex-wrap items-center gap-4 text-gray-400">
+					<div className="flex cursor-pointer items-center gap-2">
+						<Image
+							alt="addPhoto"
+							height={20}
+							src={"/addImage.png"}
+							width={20}
+						/>
+						<span>Photo</span>
+					</div>
+					<div className="flex cursor-pointer items-center gap-2">
+						<Image
+							alt=""
+							height={20}
+							src="/addVideo.png"
+							width={20}
+						/>
+						Video
+					</div>
+					<div className="flex cursor-pointer items-center gap-2">
+						<Image alt="" height={20} src="/poll.png" width={20} />
+						Poll
+					</div>
+					<div className="flex cursor-pointer items-center gap-2">
+						<Image
+							alt=""
+							height={20}
+							src="/addevent.png"
+							width={20}
+						/>
+						Event
+					</div>
+				</div>
+			</div>
+		</div>
+	)
 }
