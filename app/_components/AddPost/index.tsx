d1c3bebae08ad38fc9335cfb44950f5e3cef09c5
Kasra <kasra.ghasemi@evolute.app>
2024-07-29 23:45:51 +0200

    feat: 🎸 add basic api routes

--- app/_components/AddPost/index.tsx
+++ app/_components/AddPost/index.tsx
@@ -1,9 +1,13 @@
+import { auth } from "@clerk/nextjs/server"
 import Image from "next/image"
 import React from "react"
 
+import prisma from "@/_lib/client"
 import faker from "@/_utils/fakerConfig"
 
 export const AddPost = () => {
+	const { userId } = auth()
+
 	return (
 		<div className="flex justify-between gap-4 rounded-lg bg-white p-4 text-sm shadow-md">
 			{/*Avatar*/}
@@ -17,11 +21,11 @@ export const AddPost = () => {
 			{/*Post*/}
 			<div className="flex-1">
 				{/*Text input*/}
-				<div className="flex gap-4">
+				<form className="flex gap-4">
 					<textarea
 						className="flex-1 rounded-lg bg-slate-100 p-2"
 						id=""
-						name=""
+						name="desc"
 						placeholder={"What is on your mind?"}
 					></textarea>
 					<Image
@@ -31,7 +35,8 @@ export const AddPost = () => {
 						src={"/emoji.png"}
 						width={20}
 					/>
-				</div>
+					<button>Send</button>
+				</form>
 				{/*Post option*/}
 				<div className="mt-4 flex flex-wrap items-center gap-4 text-gray-400">
 					<div className="flex cursor-pointer items-center gap-2">
