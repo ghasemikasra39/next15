2e6b5a3fdab9d925be142c0a92889ab8f1cb8448
Kasra <kasra.ghasemi@evolute.app>
2024-08-06 17:56:21 +0200

    feat: 🎸 add useFormStatus

--- app/_components/RightMenu/components/UserInfoCard/components/UpdateUser/components/UpdateButton/index.tsx
+++ app/_components/RightMenu/components/UserInfoCard/components/UpdateUser/components/UpdateButton/index.tsx
@@ -1,9 +1,15 @@
 "use client"
 
+import { useFormStatus } from "react-dom"
+
 export const UpdateButton = () => {
+	const { pending } = useFormStatus()
 	return (
-		<button className="mt-2 rounded-md bg-blue-500 p-2 text-white disabled:cursor-not-allowed disabled:bg-opacity-50">
-			Update
+		<button
+			className="mt-2 rounded-md bg-blue-500 p-2 text-white disabled:cursor-not-allowed disabled:bg-opacity-50"
+			disabled={pending}
+		>
+			{pending ? "Updating ..." : "Update"}
 		</button>
 	)
 }
